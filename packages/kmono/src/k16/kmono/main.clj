a390f230cf1ee3a459034e703185b98ca508caff
Artem Medeu <artem.medeusheyev@gmail.com>
2023-12-07 12:46:50 +0600

    initial preparations for native build

--- packages/kmono/src/k16/kmono/main.clj
+++ packages/kmono/src/k16/kmono/main.clj
@@ -1,7 +1,16 @@
 (ns k16.kmono.main
   (:require
-   [k16.kmono.adapters.clojure-deps :as clojure-deps]))
+   [clojure.pprint :as pprint]
+   [clojure.string :as string]
+   [k16.kmono.api :as api])
+  (:gen-class))
 
 (defn -main [& args]
-  )
+  (let [params (->> args
+                    (partition-all 2)
+                    (map (fn [[k v]]
+                           [(keyword (string/replace k #"^:" "")) v]))
+                    (into {}))]
+    (pprint/pprint params)
+    (api/run params)))
 
