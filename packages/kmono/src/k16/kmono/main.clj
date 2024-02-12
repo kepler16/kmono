5c1acaf175d380f76f2aae442b02716083cf03f1
Artem Medeu <artem.medeusheyev@gmail.com>
2024-02-12 19:18:35 +0600

    fix: alias parcing for namespaced kws

--- packages/kmono/src/k16/kmono/main.clj
+++ packages/kmono/src/k16/kmono/main.clj
@@ -37,9 +37,9 @@
     :default true
     :parse-fn #(Boolean/parseBoolean %)]])
 
-(defn- parse-aliases
+(defn parse-aliases
   [aliases-arg]
-  (->> aliases-arg (re-seq #"[/a-zA-Z0-9\*]+") (mapv keyword)))
+  (->> aliases-arg (re-seq #"[/a-zA-Z0-9\*\./]+") (mapv keyword)))
 
 (defn- cp-option
   [default-value description]
