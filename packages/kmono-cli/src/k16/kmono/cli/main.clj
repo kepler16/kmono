8e099baeb4ad7aa9ed2c1eb24e170fc2d0748a4f
Julien Vincent <m@julienvincent.io>
2024-09-05 00:48:15 +0200

    Add version to cli help text

--- packages/kmono-cli/src/k16/kmono/cli/main.clj
+++ packages/kmono-cli/src/k16/kmono/cli/main.clj
@@ -1,18 +1,24 @@
 (ns k16.kmono.cli.main
   (:require
+   [babashka.process :as proc]
    [cli-matic.core :as cli]
+   [clojure.string :as str]
    [k16.kmono.cli.commands.cp :as commands.cp]
-   [k16.kmono.cli.commands.repl :as commands.repl]
    [k16.kmono.cli.commands.exec :as commands.run]
+   [k16.kmono.cli.commands.repl :as commands.repl]
    [k16.kmono.cli.commands.run :as commands.tool])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
+(defmacro version []
+  (let [res (proc/sh (str/split "git describe --abbrev=0 --tags" #" "))]
+    (str/replace (str/trim (:out res)) #"v" "")))
+
 (def cli-configuration
   {:command "kmono"
    :description "A cli for managing clojure (mono)repos"
-   :version "0.0.0"
+   :version (version)
    :opts [{:as "Run commands as if in this directory"
            :option "dir"
            :short "d"
