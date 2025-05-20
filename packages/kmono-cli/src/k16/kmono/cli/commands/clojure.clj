d01394dfabe9577dab29f26fc13d7e59bc5f321f
Julien Vincent <m@julienvincent.io>
2025-05-20 13:19:02 +0100

    Allow filtering included packages in `clojure` cmd
    Add the various filter command opts used in `run` and `exec` to the
    `clojure` command.

--- packages/kmono-cli/src/k16/kmono/cli/commands/clojure.clj
+++ packages/kmono-cli/src/k16/kmono/cli/commands/clojure.clj
@@ -5,15 +5,35 @@
    [k16.kmono.cli.common.context :as common.context]
    [k16.kmono.cli.common.opts :as opts]
    [k16.kmono.core.deps :as core.deps]
+   [k16.kmono.core.graph :as core.graph]
+   [k16.kmono.core.packages :as core.packages]
    [k16.kmono.cp :as kmono.cp]
-   [k16.kmono.log :as log]))
+   [k16.kmono.log :as log]
+   [k16.kmono.version :as kmono.version]))
 
 (set! *warn-on-reflection* true)
 
-(defn run-clojure [{:keys [A M T X] :as props} args]
+(defn run-clojure [{:keys [filter skip-unchanged changed changed-since
+                           A M T X] :as props} args]
   (let [aliases (or A M T X)
         props (assoc props :aliases aliases)
         {:keys [root config packages]} (common.context/load-context props)
+
+        packages
+        (cond-> packages
+          filter
+          (->> (core.graph/filter-by (core.packages/name-matches? filter)))
+
+          (or changed skip-unchanged)
+          (->> (kmono.version/resolve-package-versions root)
+               (kmono.version/resolve-package-changes root)
+               (core.graph/filter-by kmono.version/package-changed?
+                                     {:include-dependents true}))
+
+          changed-since
+          (->> (kmono.version/resolve-package-changes-since root changed-since)
+               (core.graph/filter-by kmono.version/package-changed?
+                                     {:include-dependents true})))
 
         mode (cond
                A "A"
@@ -60,6 +80,11 @@
    :desc "Run an augmented clojure command"
 
    :options {:package-aliases opts/package-aliases-opt
+
+             :skip-unchanged opts/skip-unchanged-opt
+             :changed opts/changed-opt
+             :changed-since opts/changed-since-opt
+             :filter opts/package-filter-opt
 
              :A {:desc "Aliases"
                  :parse-fn opts/parse-bool-or-aliases}
