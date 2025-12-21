kepler16/kmono

commit 3e259e35ef1423c1c0f2f14d62e53dbf2e88dab5

Add simpler API for resolving kmono workspace
This introduces a new `k16.kmono.workspace/resolve-workspace-context!`
which composes all the lower-level API calls required in order to fully
resolve a kmono workspace.

build/build.clj
@@ -4,11 +4,9 @@
    [clojure.tools.build.api :as b]
    [k16.kaven.deploy :as kaven.deploy]
    [k16.kmono.build :as kmono.build]
-   [k16.kmono.core.config :as core.config]
-   [k16.kmono.core.fs :as core.fs]
    [k16.kmono.core.graph :as core.graph]
-   [k16.kmono.core.packages :as core.packages]
-   [k16.kmono.git.tags :as git.tags]))
+   [k16.kmono.git.tags :as git.tags]
+   [k16.kmono.workspace :as kmono.workspace]))
 
 (defn- get-latest-version [dir]
   (some (fn [tag]
@@ -17,12 +15,9 @@
         (git.tags/get-sorted-tags dir)))
 
 (defn- load-packages []
-  (let [project-root (core.fs/find-project-root!)
-        workspace-config (core.config/resolve-workspace-config project-root)
-        packages (->> (core.packages/resolve-packages project-root workspace-config)
-                      (core.graph/filter-by #(not (get-in % [:deps-edn :kmono/private]))))
-
-        version (get-latest-version project-root)]
+  (let [{:keys [root packages]} (kmono.workspace/resolve-workspace-context!)
+        packages (core.graph/filter-by packages #(not (get-in % [:deps-edn :kmono/private])))
+        version (get-latest-version root)]
 
     (reduce
      (fn [packages [pkg-name pkg]]

examples/workspace/build/build.clj
@@ -3,22 +3,20 @@
    [clojure.tools.build.api :as b]
    [k16.kaven.deploy :as kaven.deploy]
    [k16.kmono.build :as kmono.build]
-   [k16.kmono.core.config :as core.config]
    [k16.kmono.core.fs :as core.fs]
    [k16.kmono.core.graph :as core.graph]
-   [k16.kmono.core.packages :as core.packages]
    [k16.kmono.git.tags :as git.tags]
    [k16.kmono.version :as kmono.version]
-   [k16.kmono.version.alg.conventional-commits :as conventional-commits]))
+   [k16.kmono.version.alg.conventional-commits :as conventional-commits]
+   [k16.kmono.workspace :as kmono.workspace]))
 
 (defn load-packages [{:keys [skip-unchanged]}]
-  (let [project-root (core.fs/find-project-root)
-        workspace-config (core.config/resolve-workspace-config project-root)
+  (let [{:keys [root packages]} (kmono.workspace/resolve-workspace-context!)
 
         packages
-        (cond->> (->> (core.packages/resolve-packages project-root workspace-config)
-                      (kmono.version/resolve-package-versions project-root)
-                      (kmono.version/resolve-package-changes project-root))
+        (cond->> (->> packages
+                      (kmono.version/resolve-package-versions root)
+                      (kmono.version/resolve-package-changes root))
 
           ;; Only perform a build and/or release for packages which have changed
           ;; since their last version; or packages whose dependencies have

examples/workspace/deps.edn
@@ -7,4 +7,4 @@
                                 com.kepler16/kmono-build {:local/root "../../packages/kmono-build"}
                                 com.kepler16/kmono-core {:local/root "../../packages/kmono-core"}}
                    :ns-default build
-                   :extra-paths ["build.clj"]}}}
+                   :extra-paths ["build"]}}}

packages/kmono-core/src/k16/kmono/workspace.clj
@@ -1,0 +1,29 @@
+(ns k16.kmono.workspace
+  (:require
+   [k16.kmono.core.config :as core.config]
+   [k16.kmono.core.fs :as core.fs]
+   [k16.kmono.core.packages :as core.packages]))
+
+(defn resolve-workspace-context!
+  "A helper function for resolving the workspace context. This combines:
+
+   1.  Finding the project root
+   2.  Resolving the kmono workspace config for the project
+   3.  Resolving the initial package graph
+
+   Returns a map containing:
+
+   - `:root` - The project root
+   - `:config` - The workspace config
+   - `:packages` - The packages graph
+
+   Will throw an exception if the project root cannot be resolved, if anything
+   is incorrectly configured, or fails to resolve."
+  ([] (resolve-workspace-context! nil))
+  ([dir]
+   (let [project-root (core.fs/find-project-root! dir)
+         workspace-config (core.config/resolve-workspace-config project-root)
+         packages (core.packages/resolve-packages project-root workspace-config)]
+     {:root project-root
+      :config workspace-config
+      :packages packages})))

commit af8ef78eb0882e28330eba955cead1c766ca8323

Fix broken build pipeline introduced in 3e259e35
The previous revision 3e259e35 introduced a bug in the build-pipeline of
the project which is hereby fixed.

build/build.clj
@@ -16,7 +16,8 @@
 
 (defn- load-packages []
   (let [{:keys [root packages]} (kmono.workspace/resolve-workspace-context!)
-        packages (core.graph/filter-by packages #(not (get-in % [:deps-edn :kmono/private])))
+        packages (core.graph/filter-by #(not (get-in % [:deps-edn :kmono/private]))
+                                       packages)
         version (get-latest-version root)]
 
     (reduce